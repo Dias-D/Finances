b7f12b612b8e10c5b32d7bf6cf442c7976edbe4c
Diego Dias <diasdsdiego@gmail.com>
2022-02-09 08:40:54 -0300

    change onclick

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const Modal = {
         document.querySelector(".modal-overlay").classList.remove("active");
     },
 };
+
+document.querySelector(".open-modal").addEventListener("click", (e) => {
+    e.preventDefault();
+    Modal.open();
+});
+
+document.querySelector(".close-modal").addEventListener("click", (e) => {
+    e.preventDefault();
+    Modal.close();
+});
